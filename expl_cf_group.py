13c3d7a24b9dc354d3be5f31e4c72264905a56d9
Alexei <56193131+RoscaAlex00@users.noreply.github.com>
2021-11-27 20:22:20 +0100

    explanations for groups

---
+++ expl_cf_group.py
@@ -0,0 +1,13 @@
+def expl_least_misery(item_name):
+    return 'The recipe', item_name, 'has been recommended since we think that all of you will enjoy this recipe, ' \
+                                    'it is healthy and none of the group members have a problem with it.'
+
+
+def expl_approval_voting(item_name):
+    return 'The recipe', item_name, 'has been recommended since it is approved by the highest amount of group members,'\
+                                    'and has the highest rating among the recipes with highest approval rate.'
+
+
+def expl_most_pleasure(item_name):
+    return 'The recipe', item_name, 'has been recommended since some of you will enjoy this recipe, ' \
+                                    'because it achieves the highest individual rating.'
--- rec_group.py
+++ rec_group.py
@@ -1,30 +1,29 @@
 import pandas as pd
 from filter_healthy import apply_health_filter
 from rec_cf import user_user_cf_group
-
+from rec_cf import item_item_cf_group
 from logger import l
 from helpers_data_load import df_users, df_recipes_full
+import expl_cf_group
 
 df_recipes = None
 
 
 # todo:high add explanations
 # todo:high add evaluation
-def group_recommender(df_users, df_recipes_full, chosen_strategy):
+def group_recommender(df_users_gr, df_recipes_full_gr, chosen_strategy, chosen_useritem):
     """
+    :param chosen_useritem: 1 for UserUser, 2 for ItemItem
     :param chosen_strategy: 1 for most pleasure, 2 for approval voting, 3 for least misery
     """
-    global df_recipes
-    # data loading usually in `helpers_data_load.py` but since this takes forever, would prefer to run it only if necessary
-    # since its global will run only once per application cycle even if knn is run multiple times
-    if not df_recipes:
-        df_recipes = pd.read_csv('Data/processed/knn_recipes.csv')
-
     if chosen_strategy not in [1, 2, 3]:
         raise ValueError("Group recommender: chosen strategy has to be 1 or 2 or 3")
 
+    if chosen_useritem not in [1, 2]:
+        raise ValueError("Group recommender: chosen algorithm has to be 1 or 2")
+
     # todo:high switch this to the user provided parameters from main
-    users_ratings = df_users.groupby(['user']).count()  # count the ratings for each user
+    users_ratings = df_users_gr.groupby(['user']).count()  # count the ratings for each user
     selected = users_ratings['rating'] > 30  # keep only 30 + ratings
     selected_users = users_ratings.loc[selected]
     random_selected = selected_users.sample(n=10)
@@ -34,16 +33,16 @@ def group_recommender(df_users, df_recipes_full, chosen_strategy):
     # iloc select by index, since our dataframe only has one row we read it from the index 0
     group_users = list(select_column_df)
 
-    group_ratings = df_users.loc[df_users['user'].isin(group_users)]
-    total_recipes = set(df_recipes.index.tolist())
-    num_ratings_df = df_users.groupby(['item']).count()
+    group_ratings = df_users_gr.loc[df_users_gr['user'].isin(group_users)]
+    total_recipes = set(df_recipes_full_gr.index.tolist())
+    num_ratings_df = df_users_gr.groupby(['item']).count()
     considered_recipes = set(num_ratings_df.loc[num_ratings_df['user'] >= 30].reset_index()['item'])
 
     group_seen_recipes = set(group_ratings['item'].tolist())
     group_unseen_recipes = considered_recipes - group_seen_recipes
 
     l.info(f'Total amount of recipes: {len(total_recipes)}')
-    l.info(f'Recipes that have at least 20 ratings, {len(considered_recipes)}')
+    l.info(f'Recipes that have at least 30 ratings, {len(considered_recipes)}')
     l.info(f'Recipes that have been rated by the currently selected group: {len(group_seen_recipes)}')
     l.info(f'New recipes that the group didnt try yet: {len(group_unseen_recipes)}')
 
@@ -52,7 +51,10 @@ def group_recommender(df_users, df_recipes_full, chosen_strategy):
 
     # todo:med switch these variables from being provided to CF here to being found by the CF itself,
     #  i.e. better decoupling
-    group_unseen_df = user_user_cf_group(df_users, group_users, group_unseen_recipes)
+    if chosen_useritem == 1:
+        group_unseen_df = user_user_cf_group(df_users_gr, group_users, group_unseen_recipes)
+    else:
+        group_unseen_df = item_item_cf_group(df_users_gr, group_users, group_unseen_recipes)
 
     # Min-Max normalization of predicted_ratings
     max_val = group_unseen_df['predicted_rating'].max()
@@ -63,17 +65,16 @@ def group_recommender(df_users, df_recipes_full, chosen_strategy):
     group_unseen_df['predicted_rating'] *= 5
 
     if chosen_strategy == 1:
-        return strategy_most_pleasure(group_unseen_df, df_recipes_full)
+        return strategy_most_pleasure(group_unseen_df, df_recipes_full_gr)
     elif chosen_strategy == 2:
-        return strategy_approval_voting(group_unseen_df, df_recipes_full, random_selected)
+        return strategy_approval_voting(group_unseen_df, df_recipes_full_gr, random_selected)
     else:
-        return strategy_least_misery(group_unseen_df, df_recipes_full)
+        return strategy_least_misery(group_unseen_df, df_recipes_full_gr)
 
 
-def strategy_least_misery(group_unseen_df, df_recipes_full):
+def strategy_least_misery(group_unseen_df, df_recipes_full_gr):
     least_misery_df = group_unseen_df.groupby(['item']).min().reset_index()
-    # todo:high Find name of recipe from the RAW data
-    least_misery_df = least_misery_df.join(df_recipes_full['name'], on='item')
+    least_misery_df = least_misery_df.join(df_recipes_full_gr['name'], on='item')
     items_lm = least_misery_df['item'].copy()
     healthy_lm = apply_health_filter(items_lm)
 
@@ -82,26 +83,27 @@ def strategy_least_misery(group_unseen_df, df_recipes_full):
         ['item', 'predicted_rating', 'name', 'healthy']]
 
     least_misery_df = least_misery_df[least_misery_df.healthy == 1]
+    print(expl_cf_group.expl_least_misery(least_misery_df['name'].iloc[0]))
     return least_misery_df
 
 
-def strategy_most_pleasure(group_unseen_df, df_recipes_full):
+def strategy_most_pleasure(group_unseen_df, df_recipes_full_gr):
     most_pleasure_df = group_unseen_df.groupby(['item']).max().reset_index()
-    # todo:high Find name of recipe from the RAW data
     items_mp = most_pleasure_df['item'].copy()
     healthy_mp = apply_health_filter(items_mp)
 
     most_pleasure_df['healthy'] = most_pleasure_df['item'].apply(lambda x: 1 if x in healthy_mp else 0)
 
-    most_pleasure_df = most_pleasure_df.join(df_recipes_full['name'], on='item').reset_index()
+    most_pleasure_df = most_pleasure_df.join(df_recipes_full_gr['name'], on='item').reset_index()
     most_pleasure_df = most_pleasure_df.sort_values(by="predicted_rating", ascending=False).reset_index()[
         ['item', 'predicted_rating', 'name', 'healthy']]
 
     most_pleasure_df = most_pleasure_df[most_pleasure_df.healthy == 1]
+    print(expl_cf_group.expl_most_pleasure(most_pleasure_df['name'].iloc[0]))
     return most_pleasure_df
 
 
-def strategy_approval_voting(group_unseen_df, df_recipes_full, random_selected):
+def strategy_approval_voting(group_unseen_df, df_recipes_full_gr, random_selected):
     group_unseen_temp_df = group_unseen_df.copy()
     group_unseen_temp_df['voted'] = group_unseen_temp_df['predicted_rating'].apply(lambda x: 1 if x > 3.5 else 0)
     approval_df = group_unseen_temp_df.groupby(['item']).sum()
@@ -111,7 +113,10 @@ def strategy_approval_voting(group_unseen_df, df_recipes_full, random_selected):
     approval_df = approval_df[approval_df.voted == approval_df.voted.max()]
     approval_df = approval_df.sort_values(by="predicted_rating",
                                           ascending=False).reset_index()  # Get the best rated items with max approval
-    approval_df = approval_df.join(df_recipes_full['name'], on='item')
+    approval_df = approval_df.join(df_recipes_full_gr['name'], on='item')
+    print(expl_cf_group.expl_approval_voting(approval_df['name'].iloc[0]))
     return approval_df
 
 
+group_recommender(df_users, df_recipes_full, 1, 1)
+group_recommender(df_users, df_recipes_full, 3, 2)
