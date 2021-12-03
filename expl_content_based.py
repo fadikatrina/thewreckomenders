423b78cbcfbba389270d5aeedde0abbcf1b78043
Adeleimpa <56159423+Adeleimpa@users.noreply.github.com>
2021-12-03 23:04:09 +0100

    explanation knn

--- expl_content_based.py
+++ expl_content_based.py
@@ -1,4 +1,3 @@
-def indiv_CB(item_name, rate, k, f1_name, f2_name):
-    return 'You will probably rate the recipe', item_name, 'with a', rate, ' because, considering the', f1_name,\
-           'and the', f2_name, 'of this recipe, the', k, 'nearest neighbours indicate that this is ' \
-                                                         'the most probable rating'
+def indiv_CB(item_name, k, neighbours):
+    return 'The recipe', item_name, 'has been recommended to you since we think that this recipe might suit your taste'\
+           'considering these', k, 'closest recipes you highly rated:', neighbours
