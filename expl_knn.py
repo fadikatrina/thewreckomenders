fa2e16e38a6d042108de1f95aaad231f34e10899
Alexei <56193131+RoscaAlex00@users.noreply.github.com>
2021-12-05 14:11:19 +0100

    knn update

--- expl_knn.py
+++ expl_knn.py
@@ -9,5 +9,6 @@ def indiv_CB(item_name, rating):
                                      'taste')
     time.sleep(0.1)
     if int(input("Input 1 to get more details or enter to move on ") or "0"):
-        l.critical('Our recommendation algorithm considered the nr of steps and cooking time of the recipes you have '
-                   'rated before and figured that you would rate the aforementioned recipe with a rating of ' + rating)
+        l.critical(
+            'Our recommendation algorithm considered the cooking time of the recipes you have '
+            'rated before and figured that you would rate the aforementioned recipe with a rating of ' + rating)
